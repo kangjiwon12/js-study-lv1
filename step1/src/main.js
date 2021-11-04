57168b7c10e8ed9ac94f0d928e7b0837480f532f
kangjiwon12 <92039349+kangjiwon12@users.noreply.github.com>
2021-11-04 17:07:28 +0900

    feat:[step1]아이템 추가 기능 작성 완료

--- step1/src/main.js
+++ step1/src/main.js
@@ -1,1 +1,16 @@
-// 코드 작성
+document.forms.appendForm.onsubmit= function(event){
+    event.preventDefault();
+
+    var $newItem= document.createElement('li');
+    $newItem.innerHTML='
+    <p>
+      ${event.target.querySelector('input').value}
+   </p>
+   <button type="button">완료</button>
+   <button type="button">수정</button>
+   <button type="button">삭제</button> 
+   ';
+    
+  
+    document.getElementBYId("todolist").appendChild($newItem);
+}
